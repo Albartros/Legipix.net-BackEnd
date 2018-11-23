1e8115d79e8bbb84af09ca6aa3e3f25a212ddf2e
Albartros <legipix@gmail.com>
2018-11-23 21:33:02 +0100

    Routes de base pour API

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,16 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $response->withRedirect('http://legipix.net');
+});
+
+$app->group('/api', function() {
+    $this->get('', function ($request, $response, $args) {
+        return $this->renderer->render($response, 'index.phtml', $args);
+    });
 });
