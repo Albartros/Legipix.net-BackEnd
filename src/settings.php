c339d1451fedda80a63be578a3db35d181246e4d
Albartros <legipix@gmail.com>
2018-11-29 09:35:06 +0100

    Ajout de .env et début implémentation Eloquent

--- src/settings.php
+++ src/settings.php
@@ -11,9 +11,21 @@ return [
 
         // Monolog settings
         'logger' => [
-            'name' => 'slim-app',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'name'  => 'slim-app',
+            'path'  => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
+        ],
+
+        // Database settings
+        'db' => [
+            'driver'    => $_ENV['DB_CONNECTION'],
+            'host'      => $_ENV['DB_HOST'],
+            'database'  => $_ENV['DB_DATABASE'],
+            'username'  => $_ENV['DB_USERNAME'],
+            'password'  => $_ENV['DB_PASSWORD'],
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
         ],
     ],
 ];
